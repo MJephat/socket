ff203eda3e55810f373c819e0fccff08cd757bb7
MJephat <mukundijephat22@gmail.com>
2024-03-10 09:14:47 +0300

    Mongodb Connectivity and configuration

--- CS-RTCA/backend/server.js
+++ CS-RTCA/backend/server.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
-const app = express();
 dotenv.config();
+connectDB();
+const app = express();
+
 
 
 app.get('/', (req, res) => {
     res.send("Api is running!")
 })
 
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
+});
+
+
+app.get("/api/chat/:id", (req, res) => {
+    // console.log(req.params.id);
+    const singleChat = chats.find(c=>c._id === req.params.id);
+    res.send(singleChat);
+})
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, console.log(`server started on port ${PORT}!`));
+app.listen(PORT, console.log(`server started on port ${PORT}!`.yellow.bold));

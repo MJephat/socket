650eddd826a4f0e70412da8b903a46f2b6ff78db
MJephat <mukundijephat22@gmail.com>
2024-03-10 05:37:26 +0300

    created 3 schemas, user,chat,message schema

---
+++ CS-RTCA/backend/Models/chatModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const chatModel = mongoose.Schema(
+    {
+        ChatName: {type: String, trim: true},
+        isGroupChat:{type: Boolean, trim: false},
+        users: [
+            {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            },
+        ],
+        latestMessage: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"Message",
+        },
+        groupAdmin: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Chat = mongoose.model("Chat", chatModel);
+
+model.exports = Chat;

27fc6b3e6a726429702abc7e1cf968cfdae00808
MJephat <mukundijephat22@gmail.com>
2024-03-14 16:12:33 +0300

    selecting a user to start  a chat functionality

--- CS-RTCA/frontend/src/components/miscellaneous/sideDrawer.js
+++ CS-RTCA/frontend/src/components/miscellaneous/sideDrawer.js
@@ -14,7 +14,7 @@ const SideDrawer = () => {
   const [loading, setLoading] = useState(false);
   const [loadingChat, setLoadingChat] = useState();
 
-  const { user } =  ChatState();
+  const { user, setSelectedChat, chats, setChats } =  ChatState();
   const history = useHistory();
   const { isOpen, onOpen, onClose } = useDisclosure();
 
@@ -65,7 +65,33 @@ const SideDrawer = () => {
     }
   };
 
-  const accessChat=(userId)=>{}
+  const accessChat= async (userId)=>{
+    try{
+      setLoadingChat(true)
+
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.post("/api/chat", {userId}, config);
+
+      setSelectedChat(data);
+      setLoading(false);
+      onClose();
+    }catch(e){
+      toast({
+        title: "Error fetching the Chat",
+        description: e.message,
+        status:"error",
+        duration: 5000,
+        isClosable: true,
+        poaition: "bottom-left",
+      });
+
+    }
+  }
 
   return (
     <>

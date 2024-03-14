e5f0e40bccd5e2931f326dbeea4432aef5337893
MJephat <mukundijephat22@gmail.com>
2024-03-14 15:52:41 +0300

    worked on sidedrawer and search og users

--- CS-RTCA/frontend/src/components/miscellaneous/sideDrawer.js
+++ CS-RTCA/frontend/src/components/miscellaneous/sideDrawer.js
@@ -1,23 +1,77 @@
 import React, { useState } from 'react'
-import {Avatar, Box, Button, Flex, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Text, Tooltip } from '@chakra-ui/react'
+import {Avatar, Box, Button, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, Input, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Text, Tooltip, useDisclosure, useToast } from '@chakra-ui/react'
 import {BellIcon, ChevronDownIcon} from '@chakra-ui/icons'
 import { ChatState } from "../../components/context/chatProvider";
 import ProfileModal from './profileModel';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom';
+import axios from 'axios';
+import ChatLoading from '../chatLoading';
+import UserListItem from '../UserAvatar/userListItem';
 
 const SideDrawer = () => {
-  const [search, setSearch] = useState("")
-  const [searchResult, setSearchResult] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [loadingChat, setLoadingChat] = useState()
+  const [search,setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingChat, setLoadingChat] = useState();
 
   const { user } =  ChatState();
+  const history = useHistory();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const toast = useToast();
+
+  const logoutHandler = () => {
+    localStorage.removeItem("userInfo");
+    history.push("/");
+  };
   
+  const handleSearch = async () =>{
+    if(!search){
+      toast({
+        title: "please Enter something in Search",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-left",
+        
+      });
+      return
+    }
+
+    try{
+      setLoading(true);
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`
+        },
+      };
+
+      const { data } = await axios.get(`/api/user?search=${search}`, config)
+
+      setLoading(false);
+      setSearchResult(data);
+    }catch(e){
+      toast({
+        title: "Error occured!",
+        description: "Failed to Load the search results",
+        status:"error",
+        duration:5000,
+        isClosable:true,
+        position: "bottom-left"
+
+      });
+
+    }
+  };
+
+  const accessChat=(userId)=>{}
 
   return (
     <>
     
   <Box
-  d="flex"
+  display={'flex'}
   justifyContent="space-between"
   alignItems={"center"}
   bg="white"
@@ -26,7 +80,7 @@ const SideDrawer = () => {
   borderWidth="5px"
 >
   <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
-    <Button variant="ghost">
+    <Button variant="ghost" onClick={onOpen}>
       <i className="fas fa-search"></i>
       <Text d={{ base: "none", md: "flex" }} px={4}>
         Search User
@@ -60,11 +114,48 @@ const SideDrawer = () => {
         </ProfileModal>
         
           <MenuDivider />
-          <MenuItem>Logout</MenuItem>
+          <MenuItem onClick={logoutHandler}>Logout</MenuItem>
         </MenuList>
       </Menu>
     </div>
   </Box>
+
+
+  <Drawer placement='left' onClose={onClose} isOpen={isOpen}>
+    <DrawerOverlay />
+    <DrawerContent>
+      <DrawerHeader borderBottomWidth={"1px"}>Search  Users</DrawerHeader>
+    
+    <DrawerBody>
+      <Box display={'flex'} pb={2}>
+        <Input
+        placeholder='search by name or email'
+        mr={2}
+        value={search}
+        onChange={(e)=>setSearch(e.target.value)}
+        />
+        <Button 
+        onClick={handleSearch}
+        >
+          Go</Button>
+      </Box>
+
+      {loading ? (
+        <ChatLoading />
+      ): (
+        searchResult?.map(user =>(
+          <UserListItem
+          ky={user._id}
+          user={user}
+          handleFunction={()=>accessChat(user._id)}
+          />
+        ))
+      )}
+
+    </DrawerBody>
+    </DrawerContent>
+
+  </Drawer>
 
 
   </>

68a4e2d6410c198da056c99c56c180f1554c7001
MJephat <mukundijephat22@gmail.com>
2024-03-15 16:40:35 +0300

    checking group members in a chat group ui

--- CS-RTCA/frontend/src/components/miscellaneous/updateGroupChat.js
+++ CS-RTCA/frontend/src/components/miscellaneous/updateGroupChat.js
@@ -1,10 +1,23 @@
 import { ViewIcon } from '@chakra-ui/icons';
-import { Button, IconButton, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Button, IconButton, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure, useToast } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { ChatState } from '../context/chatProvider';
+import UserBadgeItem from '../UserAvatar/userBadgeItem';
 
 const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain}) => {
 
     const { isOpen, onOpen, onClose }= useDisclosure();
+    const { selectedChat, setSelectedChat, user } = ChatState();
+    
+    const [chatName, setChatName] = useState();
+    const [search, setSearch] = useState("");
+    const [searchResult, setSearchResult] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [renameLoading, setRenameLoading] = useState(false);
+
+    const toast = useToast();
+    
+    const handleRemove = ()=>{};
 
   return (
     <>
@@ -13,13 +26,27 @@ const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain}) => {
       icon={<ViewIcon />}
       onClick={onOpen}/>
 
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader 
+          fontSize={'35px'}
+          fontFamily={'Work sans'}
+          display={'flex'}
+          justifyContent={'center'}
+          >
+            {selectedChat.chatName}
+            </ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            {/* <Lorem count={2} /> */}
+          <Box  width={'100%'} display={'flex'} flexWrap={'wrap'} pb={3}>
+            {selectedChat.users.map((u)=>(
+                <UserBadgeItem
+                key={user._id}
+                user={u}
+                handleFunction={()=>handleRemove(u)} />
+            ))}
+          </Box>
           </ModalBody>
 
           <ModalFooter>
